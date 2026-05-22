9b2e3d633af37796925e870431e8f3665a519bbf
KleinerSource <KleinerSource@me.com>
2026-05-22 11:46:55 +0800

    feat(detail): compose 10 sections into MovieDetailContent

--- lib/features/movies/detail/movie_detail_content.dart
+++ lib/features/movies/detail/movie_detail_content.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
 import '../../../core/models/movie.dart';
+import 'movie_detail_actions.dart';
+import 'sections/extra_fanart_section.dart';
+import 'sections/file_paths_section.dart';
+import 'sections/hero_section.dart';
+import 'sections/info_section.dart';
+import 'sections/media_info_section.dart';
+import 'sections/plot_section.dart';
+import 'sections/related_movies_section.dart';
+import 'sections/taxonomy_section.dart';
 
 class MovieDetailContent extends StatelessWidget {
   const MovieDetailContent({super.key, required this.movie});
@@ -9,11 +18,19 @@ class MovieDetailContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      padding: const EdgeInsets.all(16),
+      padding: EdgeInsets.zero,
       children: [
-        Text(movie.title, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-        const SizedBox(height: 8),
-        if (movie.plot != null) Text(movie.plot!),
+        HeroSection(movie: movie),
+        MovieDetailActions(movie: movie),
+        InfoSection(movie: movie),
+        PlotSection(movie: movie),
+        TaxonomySection(movie: movie),
+        RelatedMoviesSection(title: '分片', movies: movie.partMovies),
+        ExtraFanartSection(movieId: movie.id),
+        RelatedMoviesSection(title: '同演员其他影片', movies: movie.actorRelatedMovies),
+        MediaInfoSection(movieId: movie.id),
+        FilePathsSection(movie: movie),
+        const SizedBox(height: 24),
       ],
     );
   }
